8fb4715526bc9ed238ceccaba997a0669d0b64db
viyan-najmaldin <najmadinviyan@gmail.com>
2022-10-02 23:55:13 +0300

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
 const init = () => {
-  
-}
-
-document.addEventListener('DOMContentLoaded', init);
+  const inputForm = document.querySelector("form");
+  inputForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const input = document.querySelector("#searchByID");
+    const x = input.value;
+    console.log(x);
+    const URL = `http://localhost:3000/movies`;
+    fetch(`${URL}/${x}`)
+      .then((res) => res.json())
+      .then((data) => {
+        const title = document.querySelector("#movieDetails > h4");
+        const summary = document.querySelector("#movieDetails > p");
+        title.innerHTML = data.title;
+        summary.innerHTML = data.summary;
+      });
+  });
+};
+document.addEventListener("DOMContentLoaded", init);
